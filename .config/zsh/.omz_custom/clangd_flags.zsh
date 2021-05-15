ff5b03c8d0b3f5f87761008f77a863cef744005b
Marwin Glaser <marwin_g@icloud.com>
2021-05-15 11:10:04 +0200

    Revert "[neovim, zsh] move clangd cli flags from init.vim to env var"
    This reverts commit 35825151d63e6eecaa169bba0f11dc71db290836.
    Currently broken due to tmux env var config issue

--- .config/zsh/.omz_custom/clangd_flags.zsh
+++
@@ -1,1 +0,0 @@
-export CLANGD_FLAGS="--background-index --completion-style=detailed --header-insertion=iwyu --header-insertion-decorators --cross-file-rename --query-driver=/usr/**/arm-none-eabi* --malloc-trim"
