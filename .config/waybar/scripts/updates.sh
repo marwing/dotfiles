6705a9791f07216a7eaf54c10be12d11947f2ac6
Marwin Glaser <marwin_g@icloud.com>
2022-02-23 16:27:07 +0100

    [waybar] add system upgrades module waybar module
    fwupd update parsing is a best guess, will get updated (if necessary)
    when updates are available the next time

---
+++ .config/waybar/scripts/updates.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+command_exists() {
+  if command -v $1 > /dev/null 2>&1; then
+    return 0;
+  else
+    return 1;
+  fi
+}
+
+get_updates_count_fwupd() {
+  if command_exists fwupdmgr; then
+    fwupdmgr --json get-updates | jq '.Devices | length'
+  fi
+}
+
+get_updates_count_pacman() {
+  if command_exists checkupdates; then
+    checkupdates | wc -l
+  elif command_exists pacman; then
+    pacman -Qu | grep -v "[ignored]" | wc -l
+  fi
+}
+
+get_updates_count_aur() {
+  if command_exists paru; then
+    paru -Qua | grep -v "[ignored]" | wc -l
+  fi
+}
+
+FWUPD_UPDATE_COUNT=$(get_updates_count_fwupd)
+PACMAN_UPDATE_COUNT=$(get_updates_count_pacman)
+AUR_UPDATE_COUNT=$(get_updates_count_aur)
+
+echo "{ \"text\": \"$((${FWUPD_UPDATE_COUNT} + ${PACMAN_UPDATE_COUNT} + ${AUR_UPDATE_COUNT}))\", \"tooltip\": \"fwupd: ${FWUPD_UPDATE_COUNT}\\npacman: ${PACMAN_UPDATE_COUNT}\\nAUR: ${AUR_UPDATE_COUNT}\" }"
