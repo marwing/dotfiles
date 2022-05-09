75df23233e68420086903d52e8231858754812de
Marwin Glaser <marwin_g@icloud.com>
2022-05-09 13:13:22 +0200

    [neovim] cmp: don't set completeopt manually, don't use insert preset
    - nvim-cmp sets completeopt by default while relevant and unsets after
    - explicitly specify every mapping for cmp in insert mode instead of
      using their preset

--- .config/nvim/lua/user/setup/nvim-cmp.lua
+++ .config/nvim/lua/user/setup/nvim-cmp.lua
@@ -5,21 +5,24 @@ if not ok or
   return
 end
 
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
-
 cmp.setup {
   snippet = {
     expand = function(args)
       require('luasnip').lsp_expand(args.body)
     end
   },
-  mapping = cmp.mapping.preset.insert({
+  mapping = {
+    ['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+    ['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  }),
+    ['<C-y>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    },
+  },
   sources = {
     { name = 'luasnip' },
     { name = 'nvim_lua' },
