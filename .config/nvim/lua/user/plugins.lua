15e5c8cb3332ed1f82229f367a2bb6b894ed305e
Marwin Glaser <marwin_g@icloud.com>
2022-05-18 21:18:47 +0200

    [neovim] lazy loading nvim-cmp

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -55,16 +55,18 @@ require('packer').startup({
     use {
       'hrsh7th/nvim-cmp',
       requires = {
+        'L3MON4D3/LuaSnip',
         'onsails/lspkind.nvim',
-        'hrsh7th/cmp-calc',
-        'saadparwaiz1/cmp_luasnip',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-path',
-        'ray-x/cmp-treesitter',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-emoji',
+        { 'hrsh7th/cmp-calc', after = 'nvim-cmp' },
+        { 'saadparwaiz1/cmp_luasnip', after = { 'nvim-cmp', 'LuaSnip' } },
+        { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' },
+        { 'hrsh7th/cmp-path', after = 'nvim-cmp' },
+        { 'ray-x/cmp-treesitter', after = 'nvim-cmp' },
+        { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
+        { 'hrsh7th/cmp-emoji', after = 'nvim-cmp' },
       },
       config = function() require('user.setup.nvim-cmp') end,
+      event = 'InsertEnter',
     }
 
     -- treesitter
