5ab46c6264a75d6ffff18c7fda28e4f6fe634487
Marwin Glaser <marwin_g@icloud.com>
2022-05-21 04:13:31 +0200

    [neovim] lualine: add lspclients component

--- .config/nvim/lua/user/setup/lualine.lua
+++ .config/nvim/lua/user/setup/lualine.lua
@@ -14,7 +14,7 @@ require('lualine').setup {
     lualine_a = { 'mode', 'spell' },
     lualine_b = { 'branch', { 'diff', source = gitsigns_diff_source }, 'diagnostics' },
     lualine_c = { { 'filename', path = 1 } },
-    lualine_x = { 'filetype' },
+    lualine_x = { 'lspclients', 'filetype' },
     lualine_y = { 'vimtex', 'encoding', 'fileformat', 'searchcount' },
     lualine_z = { 'obsession', 'wordcount', 'fileprogress' }
   },
