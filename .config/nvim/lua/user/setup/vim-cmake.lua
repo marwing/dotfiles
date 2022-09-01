33adde7a136e752fc39f41a4b78062297a22168d
Marwin Glaser <marwin_g@icloud.com>
2022-09-01 13:26:04 +0200

    [nvim] vim-cmake: use mapleader and add mapping for CMakeTest

--- .config/nvim/lua/user/setup/vim-cmake.lua
+++ .config/nvim/lua/user/setup/vim-cmake.lua
@@ -2,7 +2,8 @@ vim.g.cmake_build_dir_location = 'build'
 vim.g.cmake_link_compile_commands = 1
 vim.g.cmake_generate_options = { '-GNinja' }
 
-vim.keymap.set('n', 'cb', '<Plug>(CMakeBuild)')
-vim.keymap.set('n', 'cr', '<cmd>CMakeSwitch Release<CR>')
-vim.keymap.set('n', 'cd', '<cmd>CMakeSwitch Debug<CR>')
-vim.keymap.set('n', 'cq', '<Plug>(CMakeClose)')
+vim.keymap.set('n', '<leader>cb', '<Plug>(CMakeBuild)')
+vim.keymap.set('n', '<leader>ct', '<Plug>(CMakeTest)')
+vim.keymap.set('n', '<leader>cr', '<cmd>CMakeSwitch Release<CR>')
+vim.keymap.set('n', '<leader>cd', '<cmd>CMakeSwitch Debug<CR>')
+vim.keymap.set('n', '<leader>cq', '<Plug>(CMakeClose)')
