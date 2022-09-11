cff4285adcd64edb387f585c6b8285d82049f7f1
Marwin Glaser <marwin_g@icloud.com>
2022-09-11 12:11:34 +0200

    [neovim] neo-tree: follow_current_file = true

--- .config/nvim/lua/user/setup/neo-tree.lua
+++ .config/nvim/lua/user/setup/neo-tree.lua
@@ -1,14 +1,13 @@
 return {
   config = function()
-    local neotree = require('neo-tree')
-
-    neotree.setup {
+    require('neo-tree').setup {
       close_if_last_window = true,
       filesystem = {
         filtered_items = {
           visible = true,
         },
         use_libuv_file_watcher = true,
+        follow_current_file = true,
       },
     }
   end,
