eee66a3b24a1cad2dc2f0b71f6650dfca663a558
Marwin Glaser <marwin_g@icloud.com>
2022-09-11 11:44:26 +0200

    [neovim] lua-dev has a new and improved setup method
    github.com/folke/lua-dev.nvim@ae74bc9987638da2e122aaaf155d6519f07e6197

--- .config/nvim/lua/user/setup/lsp/lua-dev.lua
+++ .config/nvim/lua/user/setup/lsp/lua-dev.lua
@@ -1,6 +1,2 @@
-local luadev = require('lua-dev').setup {
-  runtime_path = true,
-  lspconfig = require('user.setup.lsp.overrides').default_params,
-}
-
-require('lspconfig').sumneko_lua.setup(luadev)
+require('lua-dev').setup {}
+require('lspconfig').sumneko_lua.setup(require('user.setup.lsp.overrides').default_params)
