6044b600915a1090605b4e8da5ff14346a75c1b6
Marwin Glaser <marwin_g@icloud.com>
2022-09-12 02:36:22 +0200

    [zsh] remove rustscan alias

--- .config/zsh/.omz_custom/aliases.zsh
+++ .config/zsh/.omz_custom/aliases.zsh
@@ -4,6 +4,5 @@ alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME' \
       rip='pv /dev/sr0 > $(vobcopy -I /dev/sr0 2>&1 | awk "/DVD-name/ {print \$3}").iso && eject' \
       ffmpeg="ffmpeg -hide_banner" \
       pall='for r in */; do echo --------------------------------$(basename "${r}")--------------------------------; git -C "${r}" pull; done' \
-      rustscan='docker run --rm rustscan/rustscan' \
       purgeemptydirs='find . -depth -type d -empty -delete' \
       arcsize='arc_summary | grep "ARC size"'
