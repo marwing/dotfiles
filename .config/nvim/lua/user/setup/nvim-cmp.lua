e800082827fd6684b946e814a3174cdfed21e8ba
Marwin Glaser <marwin_g@icloud.com>
2022-10-03 15:32:08 +0200

    [neovim] cmp: use cmdline search mapping for ? as well
    supported since
    https://github.com/hrsh7th/nvim-cmp@83c097699deb76b0b727c048dc46302b1afdde6e

--- .config/nvim/lua/user/setup/nvim-cmp.lua
+++ .config/nvim/lua/user/setup/nvim-cmp.lua
@@ -47,7 +47,7 @@ cmp.setup {
   },
 }
 
-cmp.setup.cmdline('/', {
+cmp.setup.cmdline({'/', '?'}, {
   mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = 'nvim_lsp_document_symbol' },
