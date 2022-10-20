4fdd6535af2d92ed98837be9f4ee03ab6502a2b8
Marwin Glaser <marwin_g@icloud.com>
2022-10-20 21:33:02 +0200

    [neovim] lsp: document_highlight: don't recreate autocmds unnecessarily

--- .config/nvim/lua/user/setup/lsp/document_highlight.lua
+++ .config/nvim/lua/user/setup/lsp/document_highlight.lua
@@ -9,36 +9,43 @@ local bufs = setmetatable({}, {
 })
 
 local function on_attach(client, bufnr)
-  if client.supports_method('textDocument/documentHighlight') then
-    table.insert(bufs[bufnr], client.id)
-    local group = vim.api.nvim_create_augroup('lsp_highlight_document_b_' .. tostring(bufnr), { clear = true })
-
-    -- highlight
-    vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-      group = group,
-      buffer = bufnr,
-      callback = vim.lsp.buf.clear_references,
-    })
-    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-      group = group,
-      buffer = bufnr,
-      callback = vim.lsp.buf.document_highlight,
-    })
-
-    -- clean up
-    vim.api.nvim_create_autocmd('LspDetach', {
-      group = group,
-      buffer = bufnr,
-      callback = function(args)
-        table.remove(bufs[args.buf], args.data.client_id)
-        if #bufs[args.buf] < 1 then
-          -- make sure there are no stale highlights sitting in the buffer that would otherwise never be removed
-          vim.lsp.buf.clear_references()
-          vim.api.nvim_del_augroup_by_id(group)
-        end
-      end,
-    })
+  if not client.supports_method('textDocument/documentHighlight') then
+    return
   end
+
+  table.insert(bufs[bufnr], client.id)
+  if #bufs[bufnr] > 1 then
+    -- autocmds are already created for previous client
+    return
+  end
+
+  local group = vim.api.nvim_create_augroup('lsp_highlight_document_b_' .. tostring(bufnr), { clear = true })
+
+  -- highlight
+  vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+    group = group,
+    buffer = bufnr,
+    callback = vim.lsp.buf.clear_references,
+  })
+  vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+    group = group,
+    buffer = bufnr,
+    callback = vim.lsp.buf.document_highlight,
+  })
+
+  -- clean up
+  vim.api.nvim_create_autocmd('LspDetach', {
+    group = group,
+    buffer = bufnr,
+    callback = function(args)
+      table.remove(bufs[args.buf], args.data.client_id)
+      if #bufs[args.buf] < 1 then
+        vim.api.nvim_del_augroup_by_id(group)
+        -- make sure there are no stale highlights sitting in the buffer that would otherwise never be removed
+        vim.lsp.buf.clear_references()
+      end
+    end,
+  })
 end
 
 return {
