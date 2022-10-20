2cd06061da495e8893a31c419a6288e99833afd4
Marwin Glaser <marwin_g@icloud.com>
2022-10-20 22:25:46 +0200

    [neovim] neodev: add lua-language-server settings

--- .config/nvim/lua/user/setup/lsp/neodev.lua
+++ .config/nvim/lua/user/setup/lsp/neodev.lua
@@ -1,2 +1,13 @@
 require('neodev').setup {}
-require('lspconfig').sumneko_lua.setup(require('user.setup.lsp.overrides').default_params)
+require('lspconfig').sumneko_lua.setup(require('user.setup.lsp.overrides').default_params {
+  settings = {
+    Lua = {
+      completion = {
+        callSnippet = 'Replace',
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
