1132fd1d6c354823ee50fd84b4b5f105d89e83bb
Marwin Glaser <marwin_g@icloud.com>
2023-02-12 13:41:30 +0100

    [neovim] update rust-tools config

--- .config/nvim/lua/user/setup/lsp/rust-tools.lua
+++ .config/nvim/lua/user/setup/lsp/rust-tools.lua
@@ -1,6 +1,17 @@
 require('rust-tools').setup {
-  server = require('user.setup.lsp.overrides').default_params,
-  tools = {
-    hover_with_actions = false,
+  server = require('user.setup.lsp.overrides').default_params {
+    settings = {
+      ['rust-analyzer'] = {
+        checkOnSave = {
+          command = 'clippy',
+        },
+        semanticHighlighting = {
+          strings = {
+            -- This overwrites treesitter injections (regex, etc)
+            enable = false,
+          },
+        },
+      },
+    },
   },
 }
