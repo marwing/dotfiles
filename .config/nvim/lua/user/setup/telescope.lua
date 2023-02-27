5e9d2ee1707e7a3c1049657700a4c51ead0e688e
Marwin Glaser <marwin_g@icloud.com>
2023-02-27 20:33:01 +0100

    [neovim] lazy load telescope

--- .config/nvim/lua/user/setup/telescope.lua
+++ .config/nvim/lua/user/setup/telescope.lua
@@ -1,28 +1,53 @@
+local function lazy_lsp_handlers()
+  -- These are the methods that telescope-lsp-handlers sets handlers for.
+  -- They should get updated when necessary
+  local handlers = {
+    'textDocument/declaration',
+    'textDocument/definition',
+    'textDocument/implementation',
+    'textDocument/typeDefinition',
+    'textDocument/references',
+    'textDocument/documentSymbol',
+    'workspace/symbol',
+    'callHierarchy/incomingCalls',
+    'callHierarchy/outgoingCalls',
+    'textDocument/codeAction',
+  }
+
+  for _, req in ipairs(handlers) do
+    vim.lsp.handlers[req] = function(...)
+      require('lazy').load { plugins = { 'telescope.nvim' } }
+      return vim.lsp.handlers[req](...)
+    end
+  end
+end
+
 return {
   'nvim-telescope/telescope.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'make',
-    },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     'gbrlsnchs/telescope-lsp-handlers.nvim',
   },
+  init = lazy_lsp_handlers,
   config = function()
     local telescope = require('telescope')
     telescope.load_extension('fzf')
-    telescope.load_extension('lsp_handlers') -- TODO: replace with custom implementation to lazy load telescope
-    telescope.load_extension('notify')
+    telescope.load_extension('lsp_handlers')
 
-    local map = require('user.utils').keymap('<leader>f', 'n')
-    local tb = require('telescope.builtin')
-    map('b', tb.builtin, { desc = 'Telescope builtin' })
-    map('r', tb.resume, { desc = 'Telescope resume' })
-    map('f', tb.find_files, { desc = 'Telescope find_files' })
-    map('l', function()
-      tb.buffers { sort_lastused = true }
-    end, { desc = 'Telescope buffers' })
-    map('g', tb.live_grep, { desc = 'Telescope live_grep' })
+    -- nvim-notify loads it's telescope extension automatically when it is first used after telescope
+    -- this is only necessary to load the extension if telescope is loaded after nvim-notify
+    if package.loaded['notify'] then
+      telescope.load_extension('notify')
+    end
   end,
-  event = 'VeryLazy',
+  -- stylua: ignore
+  keys = {
+    { '<leader>fb', function() require('telescope.builtin').builtin() end, desc = 'Telescope builtin' },
+    { '<leader>fr', function() require('telescope.builtin').resume() end, desc = 'Telescope resume' },
+    { '<leader>ff', function() require('telescope.builtin').find_files() end, desc = 'Telescope find_files' },
+    { '<leader>fl', function() require('telescope.builtin').buffers { sort_lastused = true } end, desc = 'Telescope buffers' },
+    { '<leader>fg', function() require('telescope.builtin').live_grep() end, desc = 'Telescope live_grep' },
+    { '<leader>fo', function() require('telescope.builtin').oldfiles() end, desc = 'Telescope old_files' },
+  },
 }
