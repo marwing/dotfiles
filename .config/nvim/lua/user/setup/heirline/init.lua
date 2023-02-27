d754c573581051c908c9bab6805ef33784cd36ca
Marwin Glaser <marwin_g@icloud.com>
2023-02-27 18:44:24 +0100

    [neovim] heirline: move setup function out of config function

--- .config/nvim/lua/user/setup/heirline/init.lua
+++ .config/nvim/lua/user/setup/heirline/init.lua
@@ -1,47 +1,47 @@
-local function config()
-  local notify = require('user.setup.heirline.utils').notify
+local function setup(args, opts)
+  args = args or {}
+  opts = opts or {}
 
-  require('user.setup.heirline.themes.init')
+  local default_opts = { statusline = true, winbar = true, tabline = false, statuscolumn = false }
+  opts = vim.tbl_extend('force', default_opts, opts)
 
-  local function setup(args, opts)
-    args = args or {}
-    opts = opts or {}
+  local configs = {
+    statusline = opts.statusline and { fallthrough = false, hl = { fg = 'stl.fg', bg = 'stl.bg' } },
+    winbar = opts.winbar and { fallthrough = false, hl = { fg = 'wb.fg', bg = 'wb.bg' } },
+    tabline = opts.tabline and { fallthrough = false },
+    statuscolumn = opts.statuscolumn and { fallthrough = false },
+  }
 
-    local default_opts = { statusline = true, winbar = true, tabline = false, statuscolumn = false }
-    opts = vim.tbl_extend('force', default_opts, opts)
-
-    local configs = {
-      statusline = opts.statusline and { fallthrough = false, hl = { fg = 'stl.fg', bg = 'stl.bg' } },
-      winbar = opts.winbar and { fallthrough = false, hl = { fg = 'wb.fg', bg = 'wb.bg' } },
-      tabline = opts.tabline and { fallthrough = false },
-      statuscolumn = opts.statuscolumn and { fallthrough = false },
-    }
-
-    for _, config in ipairs(args) do
-      local ok, module = pcall(require, 'user.setup.heirline.configs.' .. config)
-      if ok then
-        if opts.statusline and module.statusline then
-          table.insert(configs.statusline, module.statusline)
-        end
-        if opts.winbar and module.winbar then
-          table.insert(configs.winbar, module.winbar)
-        end
-        if opts.tabline and module.tabline then
-          table.insert(configs.tabline, module.tabline)
-        end
-        if opts.statuscolumn and module.statuscolumn then
-          table.insert(configs.statuscolumn, module.statuscolumn)
-        end
-      else
-        -- stylua: ignore
-        notify('Error loading configuration "' .. config .. '"\n' ..
-        'File missing or error in configuration:\n\n' ..
-        module)
+  for _, config in ipairs(args) do
+    local ok, module = pcall(require, 'user.setup.heirline.configs.' .. config)
+    if ok then
+      if opts.statusline and module.statusline then
+        table.insert(configs.statusline, module.statusline)
       end
+      if opts.winbar and module.winbar then
+        table.insert(configs.winbar, module.winbar)
+      end
+      if opts.tabline and module.tabline then
+        table.insert(configs.tabline, module.tabline)
+      end
+      if opts.statuscolumn and module.statuscolumn then
+        table.insert(configs.statuscolumn, module.statuscolumn)
+      end
+    else
+      -- stylua: ignore
+      require('user.setup.heirline.utils').notify(
+        'Error loading configuration "' .. config .. '"\n' ..
+        'File missing or error in configuration:\n\n' ..
+        module
+      )
     end
-
-    return configs
   end
+
+  return configs
+end
+
+local function config()
+  require('user.setup.heirline.themes')
 
   local config = setup {
     -- 'help',
