dbdc322ea9062d8c9a4b6724668c3c9f88ceb58a
Marwin Glaser <marwin_g@icloud.com>
2023-03-24 03:45:22 +0100

    [neovim] gitsigns: add keymaps for navigation and hunk textobject

--- .config/nvim/lua/user/setup/gitsigns.lua
+++ .config/nvim/lua/user/setup/gitsigns.lua
@@ -7,6 +7,33 @@ return {
         gitdir = vim.env.HOME .. '/.dotfiles',
       },
     },
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+
+      -- Navigation
+      vim.keymap.set('n', ']c', function()
+        if vim.wo.diff then
+          return ']c'
+        end
+        vim.schedule(function()
+          gs.next_hunk()
+        end)
+        return '<Ignore>'
+      end, { expr = true, buffer = bufnr })
+
+      vim.keymap.set('n', '[c', function()
+        if vim.wo.diff then
+          return '[c'
+        end
+        vim.schedule(function()
+          gs.prev_hunk()
+        end)
+        return '<Ignore>'
+      end, { expr = true, buffer = bufnr })
+
+      -- Text object
+      vim.keymap.set({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { buffer = bufnr })
+    end,
   },
   event = 'VeryLazy',
 }
