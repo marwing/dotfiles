30aeb667f5c3bbaff5959f3e99658ccdfefa06ab
Marwin Glaser <marwin_g@icloud.com>
2023-09-01 21:02:45 +0200

    [neovim] disable unused providers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,11 @@
 -- disable jobcontrol (suspend) of neovim on <C-z>
 vim.keymap.set({ 'n', 'v' }, '<C-z>', '<nop>')
 
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_python3_provider = 0
+
 require('user.mappings')
 require('user.options')
 require('user.plugins')
