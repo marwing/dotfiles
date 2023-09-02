ecc633bc9bf2ae3efa77bdadd1681a5f86d871d7
Marwin Glaser <marwin_g@icloud.com>
2023-09-02 15:56:17 +0200

    [neovim] vimtex: rework config

--- .config/nvim/lua/user/setup/vimtex.lua
+++ .config/nvim/lua/user/setup/vimtex.lua
@@ -3,14 +3,12 @@ return {
   init = function()
     vim.g.tex_flavor = 'latex'
     vim.g.vimtex_view_method = 'zathura'
-    vim.g.vimtex_quickfix_mode = 0
     vim.g.vimtex_compiler_latexmk = {
-      build_dir = 'dist',
+      out_dir = 'dist',
     }
+    vim.g.vimtex_quickfix_enabled = 0
+    vim.g.vimtex_fold_enabled = 1
+    vim.g.vimtex_doc_handlers = { 'vimtex#doc#handlers#texdoc' }
+    vim.g.vimtex_doc_confirm_single = false
   end,
-  ft = {
-    'cls',
-    'tex',
-    'tikz',
-  },
 }
